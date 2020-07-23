df4591d683d84412e560b081e978008b0618efa5
RobertDavidMadsen <RobertDavidMadsen@gmail.com>
2020-07-22 20:59:19 -0600

    Updates

---
+++ precache-manifest.804fc3dd6538539819cee8851aefbb5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac2c7262f7fd9a8bdb4f18f25916148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dfaa1ffb61313dd9b2a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5e9256c8242ff801c2e",
+    "url": "/static/js/2.86fae083.chunk.js"
+  },
+  {
+    "revision": "a75ee770eeb065faa45ae4929f4eae76",
+    "url": "/static/js/2.86fae083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dfaa1ffb61313dd9b2a",
+    "url": "/static/js/main.ed94b97b.chunk.js"
+  },
+  {
+    "revision": "7d0bd7336175e0c0e645",
+    "url": "/static/js/runtime-main.728fa443.js"
+  }
+]);
